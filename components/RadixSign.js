611040c918938f5d4298daf2b6576133581240fd
Sam Potter <contact@sampotter.me>
2023-01-15 12:38:51 +1100

    Fix dialog

--- components/RadixSign.js
+++ components/RadixSign.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import * as Dialog from '@radix-ui/react-dialog';
-import '../styles/radixSign.module.css'
-import { Cross2Icon } from '@radix-ui/react-icons';
-
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import styles from "../styles/radixSign.module.css";
 
 export const RadixDialog = () => (
   <Dialog.Root>
@@ -11,41 +9,48 @@ export const RadixDialog = () => (
         Edit profile
       </button>
     </Dialog.Trigger>
+
     <Dialog.Portal>
-      <Dialog.Overlay className="DialogOverlay" />
-      <Dialog.Content className="DialogContent">
-        <Dialog.Title className="DialogTitle">Sign Up</Dialog.Title>
-        <Dialog.Description className="DialogDescription">
-          Sign up here! Click the sign up button when you have entered your details.
-        </Dialog.Description>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="username">
-            Username
-          </label>
-          <input className="Input" id="name" defaultValue="..." />
-        </fieldset>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="password">
-            Password
-          </label>
-          <input className="Input" id="username" defaultValue="..." />
-        </fieldset>
-        <fieldset className='Fieldset'>
-          <label className='Label' htmlFor='email'>
-            Email 
-          </label>
-          <input className='Input' id='email' defaultValue='...' />
-        </fieldset>
-        <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}>
+      <Dialog.Overlay className={styles.DialogOverlay}>
+        <Dialog.Content className={styles.DialogContent}>
+          <Dialog.Title className={styles.DialogTitle}>Sign Up</Dialog.Title>
+
+          <Dialog.Description className={styles.DialogDescription}>
+            Sign up here! Click the sign up button when you have entered your details.
+          </Dialog.Description>
+
+          <fieldset className={styles.Fieldset}>
+            <label className={styles.Label} htmlFor="username">
+              Username
+            </label>
+            <input className="Input" id="name" defaultValue="..." />
+          </fieldset>
+
+          <fieldset className={styles.Fieldset}>
+            <label className={styles.Label} htmlFor="password">
+              Password
+            </label>
+            <input className="Input" id="username" defaultValue="..." />
+          </fieldset>
+
+          <fieldset className={styles.Fieldset}>
+            <label className={styles.Label} htmlFor="email">
+              Email
+            </label>
+            <input className={styles.Input} id="email" defaultValue="..." />
+          </fieldset>
+
+          <div style={{ display: "flex", marginTop: 25, justifyContent: "flex-end" }}>
+            <Dialog.Close asChild>
+              <button className={styles.button}>Sign Up</button>
+            </Dialog.Close>
+          </div>
+
           <Dialog.Close asChild>
-            <button className="Button green">Sign Up</button>
+            <button className={styles.IconButton} aria-label="Close"></button>
           </Dialog.Close>
-        </div>
-        <Dialog.Close asChild>
-          <button className="IconButton" aria-label="Close">
-          </button>
-        </Dialog.Close>
-      </Dialog.Content>
+        </Dialog.Content>
+      </Dialog.Overlay>
     </Dialog.Portal>
   </Dialog.Root>
 );
